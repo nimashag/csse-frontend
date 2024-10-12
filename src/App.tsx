b9735c6853e49eb81fa5d6c7e35669537fed0cc1
Nimasha Gamage <nimasha.piyumini@gmail.com>
2024-10-12 19:31:02 +0530

    Add ministry pages, routes, add hospital form

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import './App.css'
 import LoginSignup from './pages/PatientLogin'
 import Dashboard from './pages/PatientDashboard'
 import AdminLogin from './pages/AdminLogins'
+import AddHospital from './pages/ministry_pages/AddHospital.tsx'
+import AddDoctor from './pages/ministry_pages/AddDoctor.tsx'
 import LabManage from './pages/lab_tech_pages/LabManage'
 import AssignedHospital from './pages/doctor_pages/AssignedHospital'
 import RoomAllocation from './pages/doctor_pages/RoomAllocation'
@@ -17,6 +19,10 @@ function App() {
         <Route path='/' element={<LoginSignup />} ></Route>
         <Route path='/dashboard' element={<Dashboard />} ></Route>
         <Route path='/admin-login' element={<AdminLogin />} />
+
+        {/* Ministry Routes */}
+        <Route path='/add-hospital' element={<AddHospital />} />
+        <Route path='/add-doctor' element={<AddDoctor />} />
 
         {/* Lab Tech Routes */}
         <Route path='/lab-manage' element={<LabManage />} />
